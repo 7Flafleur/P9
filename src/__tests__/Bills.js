c2caa00ce427089f8ddbfd9fd59b44b4675f2a45
7Flafleur <7nb@hotmail.fr>
2024-04-04 14:37:25 +0200

    bills = billsui

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -13,6 +13,7 @@ import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+    document.body.innerHTML = BillsUI({ data: bills }) //when on Bills page, body always needs to be rendered with billsUI
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -31,11 +32,19 @@ describe("Given I am connected as an employee", () => {
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+      // document.body.innerHTML = BillsUI({ data: bills })
       const dates = dom.screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
+    // test("Clicking on newBills button should redirect me to NewBills page",()=>{
+
+    // })
+
+
+
+
   })
 })
