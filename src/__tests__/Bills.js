d98013e50446fd9c3fd900a7a6602acb35ecec7c
7Flafleur <7nb@hotmail.fr>
2024-04-04 14:28:34 +0200

    imports and expect

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import '@testing-library/jest-dom/extend-expect';   // import
+import * as dom from "@testing-library/dom"  // import entire library
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
@@ -23,14 +24,15 @@ describe("Given I am connected as an employee", () => {
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      await dom.waitFor(() => dom.screen.getByTestId('icon-window'))
+      const windowIcon = dom.screen.getByTestId('icon-window')
       //to-do write expect expression
+      expect(windowIcon).toHaveClass('active-icon')
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const dates = dom.screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
