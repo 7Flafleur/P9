b72eeb4d5d0e25d1b5576594d0b629d1f8e7d910
7Flafleur <7nb@hotmail.fr>
2024-04-10 15:35:21 +0200

    comments store

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,10 +1,10 @@
 
 const jsonOrThrowIfError = async (response) => {
-  if(!response.ok) throw new Error((await response.json()).message)
+  if(!response.ok) throw new Error((await response.json()).message)  // if status within 200-299, return response body as JSON
   return response.json()
 }
 
-class Api {
+class Api {                 //defines methods for making http requests
   constructor({baseUrl}) {
     this.baseUrl = baseUrl;
   }
@@ -22,33 +22,36 @@ class Api {
   }
 }
 
-const getHeaders = (headers) => {
-  const h = { }
-  if (!headers.noContentType) h['Content-Type'] = 'application/json'
-  const jwt = localStorage.getItem('jwt')
-  if (jwt && !headers.noAuthorization) h['Authorization'] = `Bearer ${jwt}`
+const getHeaders = (headers) => {   //generates http headers
+  const h = { }      //empty object
+  if (!headers.noContentType) h['Content-Type'] = 'application/json'  // add content type value if undefinde, null, NaN, 0 or false
+  const jwt = localStorage.getItem('jwt')  //retrieve token for identification
+  if (jwt && !headers.noAuthorization) h['Authorization'] = `Bearer ${jwt}` //if jwt exists and noauthorization property is nut true, set Authorization property to h with value bearer jwt
+  //return object with combined properties of h and header
   return {...h, ...headers}
 }
 
-class ApiEntity {
+class ApiEntity {                                               
   constructor({key, api}) {
     this.key = key;
     this.api = api;
   }
-  async select({selector, headers = {}}) {
+  //async means function, means method, CRUD, 
+
+  async select({selector, headers = {}}) {               //retrieves a specific entity (like a user or a bill) from the API, based on the selector provided
     return await (this.api.get({url: `/${this.key}/${selector}`, headers: getHeaders(headers)}))
   }
   //function that retrieves bills
-  async list({headers = {}} = {}) {
+  async list({headers = {}} = {}) {           //retrieves all entities of a certain type (like all users or all bills) from the API.
     return await (this.api.get({url: `/${this.key}`, headers: getHeaders(headers)}))
   }
-  async update({data, selector, headers = {}}) {
+  async update({data, selector, headers = {}}) {  //updates a specific entity on the API, based on the selector provided.
     return await (this.api.patch({url: `/${this.key}/${selector}`, headers: getHeaders(headers), data}))
   }
-  async create({data, headers = {}}) {
+  async create({data, headers = {}}) {   // creates a new entity on the API
     return await (this.api.post({url: `/${this.key}`, headers: getHeaders(headers), data}))
   }
-  async delete({selector, headers = {}}) {
+  async delete({selector, headers = {}}) {   //deletes a specific entity from the API, based on the selector provided.
     return await (this.api.delete({url: `/${this.key}/${selector}`, headers: getHeaders(headers)}))
   }
 }
@@ -60,14 +63,14 @@ class Store {
     this.api = new Api({baseUrl: 'http://localhost:5678'})
   }
 
-  user = uid => (new ApiEntity({key: 'users', api: this.api})).select({selector: uid})
-  users = () => new ApiEntity({key: 'users', api: this.api})
-  login = (data) => this.api.post({url: '/auth/login', data, headers: getHeaders({noAuthorization: true})})
+  user = uid => (new ApiEntity({key: 'users', api: this.api})).select({selector: uid})      //Returns a specific user from the API
+  users = () => new ApiEntity({key: 'users', api: this.api})   //users(): Returns all users from the API.
+  login = (data) => this.api.post({url: '/auth/login', data, headers: getHeaders({noAuthorization: true})}) //login(data): Sends a login request to the API.
 
-  ref = (path) => this.store.doc(path)
+  ref = (path) => this.store.doc(path) //ref(path): This method seems incomplete as this.store is not defined anywhere in the class.
 
-  bill = bid => (new ApiEntity({key: 'bills', api: this.api})).select({selector: bid})
-  bills = () => new ApiEntity({key: 'bills', api: this.api})
+  bill = bid => (new ApiEntity({key: 'bills', api: this.api})).select({selector: bid})  //bill(bid): Returns a specific bill from the API.
+  bills = () => new ApiEntity({key: 'bills', api: this.api}) //bills(): Returns all bills from the API.
 }
 
 export default new Store()
